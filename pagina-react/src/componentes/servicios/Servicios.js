a4945ffe54326a727e6063b1f7102a0acd6bfe24
Raimon <ramonkoudsi@gmail.com>
2020-02-25 14:40:49 -0400

    cambios

--- pagina-react/src/componentes/servicios/Servicios.js
+++ pagina-react/src/componentes/servicios/Servicios.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import Footer from '../footer/Footer';
 import Menu from '../menu/Menu';
+import Card from './cartas/Cartas';
 
 import producto from './producto.png';
 
 
-import './collapse/collapse.css';
+import './collapse.css';
  
 class Servicios extends React.Component {
 
@@ -30,9 +31,9 @@ class Servicios extends React.Component {
 				</section>
 
 
-        <div class="accordion mb-5" id="accordionExample">
+        <div class="accordion mb-5 ml-2 mr-2" id="accordionExample">
 
-          <div class="card">
+          <div class="card" id="carta-uno">
             <div class="card-header btn" id="headingOne" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
               <h2 class="mb-0" id="font">
                   Juguetería
@@ -42,9 +43,7 @@ class Servicios extends React.Component {
               <div class="card-body">
                 
 
-
-
-
+              
 
 
                 
@@ -53,7 +52,8 @@ class Servicios extends React.Component {
           </div>
 
 
-          <div class="card">
+
+          <div class="card" id="carta-dos">
             <div class="card-header btn" id="headingTwo" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
               <h2 class="mb-0" id="font">
                   Papelería
@@ -61,13 +61,16 @@ class Servicios extends React.Component {
             </div>
             <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
               <div class="card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                
+                
+
+
               </div>
             </div>
           </div>
 
 
-          <div class="card">
+          <div class="card" id="carta-tres">
             <div class="card-header btn" id="headingThree" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
               <h2 class="mb-0" id="font">
                   Ferretería
@@ -75,13 +78,16 @@ class Servicios extends React.Component {
             </div>
             <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
               <div class="card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+
+
+
+
               </div>
             </div>
           </div>
 
 
-          <div class="card">
+          <div class="card" id="carta-cuatro">
             <div class="card-header btn" id="headingFour" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
               <h2 class="mb-0" id="font">
                   Joyería
@@ -89,13 +95,16 @@ class Servicios extends React.Component {
             </div>
             <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
               <div class="card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+
+
+
+
               </div>
             </div>
           </div>
 
 
-          <div class="card">
+          <div class="card" id="carta-cinco">
             <div class="card-header btn" id="headingFive" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
               <h2 class="mb-0" id="font">
                   Mucho más
@@ -103,7 +112,9 @@ class Servicios extends React.Component {
             </div>
             <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordionExample">
               <div class="card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+
+
+
               </div>
             </div>
           </div>
---
+++ pagina-react/src/componentes/servicios/cartas/Cartas.js
@@ -0,0 +1,24 @@
+import React from 'react';
+ 
+class Card extends React.Component {
+ 
+    render() {
+ 
+  	    return (
+
+<div class="card" style="width: 18rem;" id="card" data-ride="card">
+  <img src="..." class="card-img-top" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+        )
+
+    }
+
+}
+   
+export default Card;
